b6376cd3bdea3038be8e4217e8a94e6153ce3279
Doni Taolin <benyamin.perkasa@ti.ukdw.ac.id>
2019-09-08 23:59:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihanlayout/Main2Activity.java
@@ -0,0 +1,33 @@
+package com.example.latihanlayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private Button btnAbout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        btnAbout = (Button) findViewById(R.id.btnAbout);
+        btnAbout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivityAbout();
+            }
+        });
+    }
+
+    public void openActivityAbout(){
+        Intent intent = new Intent(this, aboutActivity.class);
+        startActivity(intent);
+
+    }
+}
